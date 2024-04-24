b7678e17dcda12d236d36c50affd7829b970cf17
Mateusz Wejman <mateusz.piotr.wejman@gmail.com>
2024-04-24 11:41:58 +0200

    Finished reset

--- PokerEnv.py
+++ PokerEnv.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 from enum import Enum
+from PokerRules import Deck
 
 import numpy as np
 
@@ -27,15 +28,28 @@ class PokerEnv(gym.Env):
             'player_ind': gym.spaces.Discrete(6),
         })
 
-    def reset(self):
+    def reset(self, seed=None, options=None):
+
+        if seed is not None:
+            np.random.seed(seed)
 
         self.players_playing = np.array([True] * 6)
         self.money = np.array([INITIAL_MONEY] * 6)
         self.round_bets = np.zeros((4, 6), dtype='int32')
         self.round_bets[0, 0] = SMALL_BLIND
         self.round_bets[0, 1] = 2 * SMALL_BLIND
+        self.money[0] -= SMALL_BLIND
+        self.money[1] -= 2 * SMALL_BLIND
         self.player_ind = np.zeros(6, dtype='int32')
         self.player_ind[0] = 1
+
+        self.deck = Deck()
+        self.deck.shuffle()
+
+        self.user_hands = [self.deck.draw_n(2) for _ in range(6)]
+        self.table_cards = []
+
+        self.round_index = 0
 
 
         return {
