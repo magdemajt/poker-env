2a358e9b3dc126d1da15c495d8f9446da11d794b
Mateusz Wejman <mateusz.piotr.wejman@gmail.com>
2024-05-28 19:21:47 +0200

    Added model configuration

--- model.py
+++ model.py
@@ -1,48 +1,23 @@
-from keras import Sequential
-from keras.src.layers import Dense, Activation
-from rl.agents import DQNAgent
-from rl.memory import SequentialMemory
-from rl.policy import BoltzmannQPolicy
-from torch.nn import Flatten
-from torch.optim import Adam
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.utils.framework import try_import_torch
 
+torch, nn = try_import_torch()
 
-def initialize_model(env):
+class CustomModel(TorchModelV2, nn.Module):
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config, name)
+        nn.Module.__init__(self)
 
-    nb_actions = env.action_space.n
+        self.fc1 = nn.LSTM(obs_space.shape[0], 64, 2)
+        self.fc2 = nn.LSTM(64, 64, 2)
+        self.fc3 = nn.Linear(64, num_outputs)
 
-    model = Sequential()
+    def forward(self, input_dict, state, seq_lens):
+        x = torch.relu(self.fc1(input_dict["obs"].float()))
+        x = torch.relu(self.fc2(x))
+        output = self.fc3(x)
+        return output, state
 
-    model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-    model.add(Dense(16))
-    model.add(Activation('relu'))
-    model.add(Dense(16))
-    model.add(Activation('relu'))
-    model.add(Dense(16))
-    model.add(Activation('relu'))
-    model.add(Dense(nb_actions, activation='linear'))
-
-    memory = SequentialMemory(limit=50000, window_length=1)
-    policy = BoltzmannQPolicy()
-    # enable the dueling network
-    # you can specify the dueling_type to one of {'avg','max','naive'}
-    dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
-                   enable_dueling_network=True, dueling_type='avg', target_model_update=1e-2, policy=policy)
-    dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-
-    return dqn
-
-def save_model(model, path):
-    model.save_weights(path, overwrite=True)
-
-def load_model(model, path):
-    model.load_weights(path)
-    return model
-
-def train_model(model, env, nb_steps):
-    model.fit(env, nb_steps=nb_steps, visualize=False, verbose=2)
-    return model
-
-def test_model(model, env, nb_episodes):
-    model.test(env, nb_episodes=nb_episodes, visualize=False)
-    return model
+# Register the custom model in ModelCatalog
+ModelCatalog.register_custom_model("my_model", CustomModel)
